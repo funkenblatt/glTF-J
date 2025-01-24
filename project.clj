7efc1dfd75a34fca1a723e0bd8f406b3ce9c8649
Jason Feng <jfeng1985@gmail.com>
2025-01-23 20:26:41 -0800

    Update description and URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.creedvi.utils/gltfj "0.0.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Java utils for reading GLTF files."
+  :url "https://github.com/CreedVI/gltf-J"
   :license {:name "Apache License 2.0"
             :url "https://apache.org/licenses/LICENSE-2.0"}
   :dependencies [[com.fasterxml.jackson.core/jackson-core "2.15.2"]
