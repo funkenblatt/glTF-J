89641263a140e968d51234484366dabc88a52e93
Jason Feng <jfeng1985@gmail.com>
2025-01-23 20:24:42 -0800

    Add leiningen project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject com.creedvi.utils/gltfj "0.0.1"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Apache License 2.0"
+            :url "https://apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[com.fasterxml.jackson.core/jackson-core "2.15.2"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.15.2"]]
+  :java-source-paths ["src"])
